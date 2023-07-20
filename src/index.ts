f4bc044bd5c24bddc6544b5c782d90bb2b5ebb36
David Flanagan <david@rawkode.dev>
2023-07-20 11:02:39 +0100

    fix: export correct function for consumption

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,1 @@
-import { CloudflareImageService } from "./service";
-import { PluginOptions } from "./types";
-
-export { PluginOptions as PluginConfig, CloudflareImageService };
+export * from "./plugin";
